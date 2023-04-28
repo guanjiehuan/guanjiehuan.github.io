1618df5a70343b07eacd470c77342f12bf2f6c34
guanjiehuan <127601529+guanjiehuan@users.noreply.github.com>
2023-04-28 22:21:19 +0800

    Add files via upload

--- use.js
+++ use.js
@@ -3,7 +3,7 @@ var context = document.getElementById("context");
 var btn = document.getElementById("btn");
 var list = document.getElementById("list");
 
-btn.addEventListener("click,function")(){
+btn.addEventListener("click",function(){
    list.innerHTML = list.innerHTML + `
    <div class="article">
    <h2>${tittle.value}</h2>
